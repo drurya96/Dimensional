081c96c34ee860bf90b72b9de1f8f0f8bb717de8
Austin Drury <drurya96@gmail.com>
2024-10-31 19:06:28 -0500

    Update conan version to 2.1.0

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ import os
 
 class DimensionalConan(ConanFile):
     name = "Dimensional"
-    version = "2.0.0"
+    version = "2.1.0"
     exports_sources = "Dimension/*"
     
     # Specify that it's a header-only library
