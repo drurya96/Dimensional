0743925cce379a204fd04d52cf30da747a830096
Austin <drurya96@gmail.com>
2025-07-23 21:11:27 -0500

    Remove old Python tools

--- autogeneration/GenerateAllFundamental.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python GenerateFundamentalDimension.py length meters
-python GenerateFundamentalDimension.py mass Grams
-python GenerateFundamentalDimension.py Time seconds
-python GenerateFundamentalDimension.py temperature Kelvin
-python GenerateFundamentalDimension.py angle radians
-python GenerateFundamentalDimension.py Amount Moles
-python GenerateFundamentalDimension.py charge Coulombs
